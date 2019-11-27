e0c83d62047644f007b8d0d6cd90b38f9e532278
softtechnoes <37590945+softtechnoes@users.noreply.github.com>
2019-11-27 12:21:38 +0530

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::post('comments', config('comments.controller') . '@store');
-Route::delete('comments/{comment}', config('comments.controller') . '@destroy');
-Route::put('comments/{comment}', config('comments.controller') . '@update');
-Route::post('comments/{comment}', config('comments.controller') . '@reply');
+Route::GET('delete-comments/{comment}', config('comments.controller') . '@destroy');
+Route::GET('comments/{comment}', config('comments.controller') . '@update');
+Route::post('reply-comments/{comment}', config('comments.controller') . '@reply');
